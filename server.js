10bfae12d583ec9f04a57b308c54731c851f3f1a
hazhdev <134780588+hazhdev@users.noreply.github.com>
2026-02-24 11:32:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const path = require("path");
 const fastify = require("fastify")({ logger: true });
 
-let tasks = [
-  { id: 1, title: "Сделать домашку", completed: false },
-  { id: 2, title: "Купить булочку", completed: true },
-  { id: 2, title: "Купить hexre", completed: true },
-];
+let tasks = [];
 let nextId = 3;
 
 fastify.register(require("@fastify/view"), {
@@ -66,61 +62,4 @@ fastify.post("/api/tasks", async (request, reply) => {
   tasks.push(task);
 
   return reply.code(201).send(task);
-});
-
-fastify.put("/api/tasks/:id", async (request, reply) => {
-  const id = Number.parseInt(request.params.id, 10);
-  if (!Number.isInteger(id)) {
-    return reply.code(400).send({ message: "Invalid id" });
-  }
-
-  const task = tasks.find((t) => t.id === id);
-  if (!task) {
-    return reply.code(404).send({ message: "Task not found" });
-  }
-
-  const { title, completed } = request.body ?? {};
-
-  if (title !== undefined) {
-    if (typeof title !== "string" || title.trim().length === 0) {
-      return reply.code(400).send({
-        message: 'Field "title" must be a non-empty string if provided.',
-      });
-    }
-    task.title = title.trim();
-  }
-
-  if (completed !== undefined) {
-    if (typeof completed !== "boolean") {
-      return reply
-        .code(400)
-        .send({ message: 'Field "completed" must be boolean if provided.' });
-    }
-    task.completed = completed;
-  }
-
-  return reply.code(200).send(task);
-});
-
-fastify.delete("/api/tasks/:id", async (request, reply) => {
-  const id = Number.parseInt(request.params.id, 10);
-  if (!Number.isInteger(id)) {
-    return reply.code(400).send({ message: "Invalid id" });
-  }
-
-  const idx = tasks.findIndex((t) => t.id === id);
-  if (idx === -1) {
-    return reply.code(404).send({ message: "Task not found" });
-  }
-
-  tasks.splice(idx, 1);
-  return reply.code(200).send({ message: "Deleted" });
-});
-
-fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log("Server started on http://localhost:3000");
 });
